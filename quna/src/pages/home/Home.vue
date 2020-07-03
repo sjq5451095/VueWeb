b56765a7be39992074e65d895def68b8a5629f4d
申继强 <sjq5451095@163.com>
2020-07-03 18:28:33 +0800

    Vue项目第二次初始化

--- quna/src/pages/home/Home.vue
+++ quna/src/pages/home/Home.vue
@@ -1,15 +1,10 @@
 <template>
-  <div id="app">
-    <router-view/>
+  <div>
+    Hellow Word
   </div>
 </template>
-
 <script>
 export default {
-  name: 'App'
+  name: 'Home'
 }
 </script>
-
-<style>
-
-</style>

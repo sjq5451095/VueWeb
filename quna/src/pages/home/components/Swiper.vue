4a7735c41d5c0fc0fcf5885c238a20da6154417b
申继强 <sjq5451095@163.com>
2020-07-03 23:29:26 +0800

    开发首页轮播

---
+++ quna/src/pages/home/components/Swiper.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="wraper">
+    <swiper :options="swiperOption" >
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-image" :src="item.imgUrl" >
+      </swiper-slide>
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [{
+        id: '001',
+        imgUrl: '../../../../static/FO7A9244.jpg'
+      }, {
+        id: '002',
+        imgUrl: '../../../../static/FO7A9251.jpg'
+      }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+      .wraper >>> .swiper-pagination-bullet-active
+        background :#fff
+      .wraper
+        width:100%
+        height: 0
+        overflow :hidden
+        padding-bottom :31.25%
+      .swiper-image
+       width:100%
+       height:2.5rem
+</style>

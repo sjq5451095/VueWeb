ccd4164b1c882e4a1511acf1075c346749ce8e18
申继强 <sjq5451095@163.com>
2020-07-04 14:12:47 +0800

    完成icon模块内容

---
+++ quna/src/pages/home/components/Icons.vue
@@ -0,0 +1,110 @@
+<template>
+<div class="icons">
+  <swiper >
+    <swiper-slide v-for="(page,index) of pages" :key="index">
+      <div class="icon"  v-for="item of page" :key="item.id">
+        <div class="icon-img">
+          <img class="icon-img-content" :src="item.imgUrl" />
+        </div>
+        <p class="icon-desc">{{item.desc}}</p>
+      </div>
+    </swiper-slide>
+  </swiper>
+</div>
+</template>
+<script>
+export default {
+  name: 'HomeIcons',
+  data () {
+    return {
+      iconList: [{
+        id: '001',
+        imgUrl: 'https://imgs.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+        desc: '热门景点'
+      }, {
+        id: '002',
+        imgUrl: 'https://imgs.qunarzz.com/piao/fusion/1804/5a/13ceb38dcf262f02.png',
+        desc: '一日游'
+      }, {
+        id: '003',
+        imgUrl: 'https://imgs.qunarzz.com/piao/fusion/1803/4c/eac47dd8def8de02.png',
+        desc: '北京园博园'
+      }, {
+        id: '004',
+        imgUrl: 'https://imgs.qunarzz.com/piao/fusion/1803/6c/9e54a8540fee0102.png',
+        desc: '故宫'
+      }, {
+        id: '005',
+        imgUrl: 'https://imgs.qunarzz.com/piao/fusion/1803/6c/9e54a8540fee0102.png',
+        desc: '圆明园'
+      }, {
+        id: '006',
+        imgUrl: 'https://imgs.qunarzz.com/piao/fusion/1803/67/9a1678221b8e0e02.png',
+        desc: '古北水镇'
+      }, {
+        id: '007',
+        imgUrl: 'https://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20193/a40ee278d67000f2a29d2e20f6a029b3.png',
+        desc: '香山公园'
+      }, {
+        id: '008',
+        imgUrl: 'https://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20193/a40ee278d67000f2a29d2e20f6a029b3.png',
+        desc: '凤凰岭'
+      }, {
+        id: '009',
+        imgUrl: 'https://imgs.qunarzz.com/piao/fusion/1803/bd/9f7b9b2b60c1502.png',
+        desc: '踏青赏花'
+      }]
+    }
+  },
+  computed: {
+    pages: function () {
+      const pages = []
+      this.iconList.forEach((item, index) => {
+        const page = Math.floor(index / 8)
+        if (!pages[page]) {
+          pages[page] = []
+        }
+        pages[page].push(item)
+      })
+      return pages
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  @import '~styles/mixings.styl'
+  .icons
+    overflow :hidden
+    height:0
+    padding-bottom :50%
+    .icon
+      position:relative
+      overflow :hidden
+      width:25%
+      height :0
+      padding-bottom :25%
+      float:left
+      .icon-img
+        position :absolute
+        top:0
+        left 0
+        right :0
+        bottom :.44rem
+        box-sizing :border-box
+        padding:.1rem
+        .icon-img-content
+          height :100%
+          display :block
+          margin: 0 auto
+      .icon-desc
+        position :absolute
+        left 0
+        right :0
+        bottom:0
+        height :.44rem
+        text-align :center
+        line-height :.44rem
+        color:$darkTextColor
+        ellipsis()
+</style>

264cc50725274320f327962eaf8b7a2d94df5bd7
申继强 <sjq5451095@163.com>
2020-07-04 22:20:59 +0800

    完成城市Ajax数据

--- quna/src/pages/city/components/List.vue
+++ quna/src/pages/city/components/List.vue
@@ -12,39 +12,15 @@
         <div class="area">
           <div class="title border-topbottom">热门城市</div>
           <div class="button-list">
-            <div class="button-wrapper">
-              <div class="button">北京</div>
+            <div class="button-wrapper" v-for="item of hot" :key="item.id">
+              <div class="button">{{item.name}}</div>
             </div>
           </div>
         </div>
-        <div class="area">
-          <div class="title border-topbottom">A</div>
+        <div class="area" v-for="(item,key) of cities"  :key="key">
+          <div class="title border-topbottom">{{key}}</div>
           <div class="item-list">
-            <div class="item border-bottom">阿拉尔</div>
-            <div class="item border-bottom">阿拉尔</div>
-            <div class="item border-bottom">阿拉尔</div>
-            <div class="item border-bottom">阿拉尔</div>
-            <div class="item border-bottom">阿拉尔</div>
-            <div class="item border-bottom">阿拉尔</div>
-            <div class="item border-bottom">阿拉尔</div>
-            <div class="item border-bottom">阿拉尔</div>
-            <div class="item border-bottom">阿拉尔</div>
-            <div class="item border-bottom">阿拉尔</div>
-            <div class="item border-bottom">阿拉尔</div>
-            <div class="item border-bottom">阿拉尔</div>
-            <div class="item border-bottom">阿拉尔</div>
-            <div class="item border-bottom">阿拉尔</div>
-            <div class="item border-bottom">阿拉尔</div>
-            <div class="item border-bottom">阿拉尔</div>
-            <div class="item border-bottom">阿拉尔</div>
-            <div class="item border-bottom">阿拉尔</div>
-            <div class="item border-bottom">阿拉尔</div>
-            <div class="item border-bottom">阿拉尔</div>
-            <div class="item border-bottom">阿拉尔</div>
-            <div class="item border-bottom">阿拉尔</div>
-            <div class="item border-bottom">阿拉尔</div>
-            <div class="item border-bottom">阿拉尔</div>
-            <div class="item border-bottom">阿拉尔</div>
+            <div class="item border-bottom" v-for="innerItem of item"  :key="innerItem.id">{{innerItem.name}}</div>
           </div>
         </div>
       </div>
@@ -55,6 +31,10 @@
 import Bscroll from 'better-scroll'
 export default {
   name: 'CityList',
+  props: {
+    cities: Object,
+    hot: Array
+  },
   mounted () {
     this.scroll = new Bscroll(this.$refs.wrapper)
   }
@@ -79,7 +59,7 @@ export default {
      right :0
      bottom :0
     .title
-      line-height :.54rem
+      line-height :.5 4rem
       background :#eee
       padding-left:.2rem
       color:#666

531d6c54b175063fa04c177c33ae39a241c51057
申继强 <sjq5451095@163.com>
2020-07-04 20:19:53 +0800

    初始化城市搜索框

--- quna/src/pages/city/City.vue
+++ quna/src/pages/city/City.vue
@@ -1,19 +1,21 @@
 <template>
   <div>
     <city-header></city-header>
+    <city-search></city-search>
   </div>
 </template>
 
 <script>
 import CityHeader from './components/Header'
+import CitySearch from './components/Search'
 export default {
   name: 'City',
   components: {
-    CityHeader: CityHeader
+    CityHeader: CityHeader,
+    CitySearch: CitySearch
   }
 }
 </script>
 
 <style lang="stylus" scoped>
-
 </style>
